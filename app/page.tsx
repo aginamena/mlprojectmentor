c70a69e470d4869c1f7c224c043fde82c207b277
aginamena <aginamena5@gmail.com>
2025-05-05 13:05:05 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,137 @@
+import Benefit from "@/components/Benefit";
+import { Container, Typography, Box, Button } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <Container style={{ paddingTop: "40px", paddingBottom: "40px" }}>
+      <Typography variant="h4" style={{ textAlign: "center" }}>
+        Stop following tutorials. Start building real-world projects.
+      </Typography>
+      <Box style={{ textAlign: "center", marginTop: "40px" }}>
+        <Link href="https://forms.gle/zGadCw1NJqmLdp6w9">
+          <Button variant="contained" size="large">
+            Sign up
+          </Button>
+        </Link>
+      </Box>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box
+        sx={{
+          display: { xs: "block", md: "flex" },
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: "50px",
+          marginBottom: "50px",
+        }}
+      >
+        <Image
+          src="/mena_agina.png"
+          width={500}
+          height={200}
+          alt="mena agina"
+        />
+        <Box sx={{ width: { xs: "100%", md: "50%" } }}>
+          <Box>
+            <Typography>
+              Hello! My name is Mena Agina — I am the founder of this website, a
+              software developer and AI enthusiast (and if you’re reading this,
+              chances are you are too).
+            </Typography>
+            <Typography style={{ marginTop: "30px", marginBottom: "30px" }}>
+              Let’s be honest: <br />
+              Many web developers get stuck in tutorial hell — watching endless
+              machine learning or coding videos without ever building something
+              real. Without hands-on projects, your learning stalls, confidence
+              dips, and your portfolio remains empty.
+            </Typography>
+            <Box>
+              That’s where this site comes in. <br />
+              <Box>
+                <Typography>
+                  My goal is to help you get back to doing what you love —
+                  building real projects. We’ll provide:
+                </Typography>
+                <Typography> (1) A clear project description</Typography>
+                <Typography>
+                  (2) Detailed design guidelines for desktop, mobile, and tablet
+                </Typography>
+                <Typography>
+                  (3) CSS styling instructions and all the key requirements
+                  you’ll need to build the app
+                </Typography>
+              </Box>
+            </Box>
+
+            <Typography style={{ marginTop: "30px", marginBottom: "30px" }}>
+              Once you’re done, the project is yours. <br />
+              Share it with friends, add it to your résumé or LinkedIn, and even
+              showcase it in our community so others can see your work.
+            </Typography>
+            <Typography>
+              I hope you’re as excited as I am. <br />
+              Sign up now so we can notify you when the next project is ready to
+              build!
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box style={{ marginTop: "30px", marginBottom: "30px" }}>
+        <Typography
+          variant="h4"
+          style={{ textAlign: "center", marginBottom: "20px" }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          What you will get
+        </Typography>
+        <Box
+          sx={{
+            display: { xs: "block", md: "flex" },
+            justifyContent: "space-between",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <Benefit
+            title="Real-World AI Projects "
+            description=" Work on projects that mimic real startup
+          challenges—no step-by-step instructions."
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <Benefit
+            title="No More Hand-Holding"
+            description="You won’t follow a tutorial. You’ll face prompts and specs that push you to build from scratch."
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Benefit
+            title="Build a Real Portfolio project"
+            description="Showcase projects that prove your skills—perfect for hiring
+          managers and tech recruiters"
+          />
+          <Benefit
+            title="Built for Web Developers"
+            description="Our projects are tailored for devs who know React, Next.js, or similar stacks and want to start building AI projects."
+          />
+        </Box>
+      </Box>
+      <Box style={{ textAlign: "center" }}>
+        <Typography variant="h4" style={{ marginBottom: "30px" }}>
+          Who is this for
+        </Typography>
+        <Box>
+          <Typography>(1) Web developers tired of tutorial hell</Typography>
+          <Typography>
+            {" "}
+            (2) Self-taught devs looking to break into AI
+          </Typography>
+          <Typography>(3) Bootcamp grads building their portfolio</Typography>
+          <Typography> (4) Engineers transitioning into ML</Typography>
+        </Box>
+      </Box>
+      <Box style={{ textAlign: "center", marginTop: "30px" }}>
+        <Typography variant="h5" style={{ marginBottom: "30px" }}>
+          Ready to Get Out of Tutorial Hell?
+        </Typography>
+        <Link href="https://forms.gle/zGadCw1NJqmLdp6w9">
+          <Button variant="contained">Sign up</Button>
+        </Link>
+      </Box>
+    </Container>
   );
 }
