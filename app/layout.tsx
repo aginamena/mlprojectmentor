fc3b8bcb138a5b6aec581f08b4ac8245cd24c686
aginamena <aginamena5@gmail.com>
2025-05-05 13:15:15 -0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import ThemeRegistry from "@/components/ThemeRegistry";
 import type { Metadata } from "next";
+
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeRegistry>{children}</ThemeRegistry>
+        <Analytics />
       </body>
     </html>
   );
